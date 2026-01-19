23e1f7f2c1b7432effb6d463b26dd0b52999a86a
Abedulaziz01 <abduvaio@gmail.com>
2026-01-19 23:09:32 +0300

    flask web app home route

--- app.py
+++ app.py
@@ -5,4 +5,19 @@ import pandas as pd
 
 app = Flask(__name__)
 rgmodel = pickle.load(open('regression_model.pkl', 'rb'))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    print(data)
+    x = np.array(data['x'])
+    y = rgmodel.predict(x)
+    return json.dumps({'y': y.tolist()})
+
+if __name__ == '__main__':
+    app.run(debug=True)
  
